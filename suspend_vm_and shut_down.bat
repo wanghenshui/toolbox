2b9daaa27ae6f19692fd62708a1d0b1485f43016
jieyaren <jieyaren@gmail.com>
2017-06-06 14:27:47 +0800

    update this ugly motherfucker

---
+++ suspend_vm_and shut_down.bat
@@ -0,0 +1,10 @@
+@echo vm suspending  
+
+cd /d "C:\Program Files (x86)\VMware\VMware VIX\"
+vmrun.exe suspend "D:\newxp\newxp.vmx"
+
+@echo shutting down
+
+shutdown -s -t 60
+
+@echo off
--- vmheadless.bat
+++ vmheadless.bat
@@ -1,7 +1,11 @@
-@echo vm starting
+@echo u17 starting
 
 cd /d "C:\Program Files\Oracle\VirtualBox\"
 
 VBoxManage.exe startvm u17 --type headless
 
+@echo encrypt xp starting
+cd /d "C:\Program Files (x86)\VMware\VMware VIX\"
+
+vmrun.exe start "D:\newxp\newxp.vmx" nogui
 @echo off
