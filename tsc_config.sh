81880047785d1b2999dc34d436e4f87c5ca359c0
jieyaren <jieyaren@gmail.com>
2017-08-09 14:38:41 +0800

    check

--- tsc_config.sh
+++ tsc_config.sh
@@ -7,27 +7,78 @@ echo "|I don't check the Parameter Validity , so be carefull |"
 echo "--------------------------------------------------------"
 echo "eth0 `ifconfig eth0 | grep 'inet ' | sed s/^.*addr://g | sed s/Bcast.*$//g `"
 echo "eth1 `ifconfig eth1 | grep 'inet ' | sed s/^.*addr://g | sed s/Bcast.*$//g `"
-echo "TSC IP enter eth0 OR eth1 :"
-read eth
-TSC_IP=`ifconfig $eth | grep 'inet ' | sed s/^.*addr://g | sed s/Bcast.*$//g | sed s/[[:space:]]//g`
 
-echo "TSC_ID :"
-read TSC_ID
+read -p "TSC IP enter eth0 OR eth1: " eth
+if [[ $eth == "eth1" || $eth == "eth0" ]] ; then
+	#eth${sed -n "/^[0-9]\+$/p"}
+	TSC_IP=`ifconfig $eth | grep 'inet ' | sed s/^.*addr://g | sed s/Bcast.*$//g | sed s/[[:space:]]//g`
+else
+	TSC_IP='20.0.0.2'
+	echo "device  error ,exit !"
+	exit
+fi
 
-echo "MSO IP :"
-read MSO_IP
+check_number(){
+	local ID=$1
+	if [ -n "$(echo $1| sed -n "/^[0-9]\+$/p")" ];then 
+		return 0
+	else 
+		echo 'Number Error' 
+		return 1
+	fi 
+}
 
-echo "DMR mode 1 for dmr and 0 for pdt :"
-read DMR
+while true; do
+    read -p "Please enter TSC_ID: " TSC_ID
+    check_number $TSC_ID
+    [ $? -eq 0 ] && break
+done
+
+
+check_ip() {
+    local IP=$1
+    VALID_CHECK=$(echo $IP|awk -F. '$1<=255&&$2<=255&&$3<=255&&$4<=255{print "yes"}')
+    if echo $IP|grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" >/dev/null; then
+        if [ $VALID_CHECK == "yes" ]; then
+            return 0
+        else
+            echo "IP $IP not available!"
+            return 1
+        fi
+    else
+        echo "IP format error!"
+        return 1
+    fi
+}
+
+while true; do
+    read -p "Please enter MSO_IP: " MSO_IP
+    check_ip $MSO_IP
+    [ $? -eq 0 ] && break
+done
+
+read -p "DMR mode 1 for dmr and 0 for pdt :" DMR
+check_number $DMR
+if [ $? -ne 0  ] ; then
+	DMR=0
+fi
+
+if [ $DMR -gt 1 ] ; then
+	DMR=0
+	echo "input ERROR and set with PDT"
+fi
+
 datename=$(date +%Y%m%d-%H%M%S) 
 cur='/opt/local/bin/VOS/cur/data'
 if [ -e $cur/VOS.Config.db ] ; then
-cp $cur/VOS.Config.db $cur/VOS.Config.db.$datename.bak
+	cp $cur/VOS.Config.db $cur/VOS.Config.db.$datename.bak
 else
-echo "db doesn't exist"
-exit
+	echo "db doesn't exist"
+	exit
 fi	
-echo "---------------sqlite3 running--------------"
+
+echo "--------------------------------------------------------"
+echo "------------------sqlite3 running-----------------------"
 sqlite3 $cur/VOS.Config.db <<EOF
 insert or replace into Tbl_Config (PID,Tag,Key,Value,Comment) VALUES(0,"VOS", "LOGVIEW/MAXCOUNT","10","");
 --update Tbl_Config set Value='0xff004f1f' where Tag='VOS' and Key='LOG/LOGLEVEL'
@@ -44,4 +95,5 @@ update Tbl_Config set Value='$TSC_IP'  where  Tag='Agent'and Key='AGENT_IP';
 update Tbl_Config set Value='$TSC_ID'        where  Tag='Agent'and Key='TSC_ID';
 EOF
 
-echo '----------------finished------------------'
+echo "-------------------------finished-----------------------"
+echo "--------------------------------------------------------"
