bd17a2bd4d3bd03253eb770dd1cd0da457f0d714
jieyaren <jieyaren@gmail.com>
2017-11-03 15:21:40 +0800

    将当前文件夹下所有word转成pdf

--- word2pdf.py
+++ word2pdf.py
@@ -1,14 +1,26 @@
+#!/usr/bin python3
+# -*- coding:utf-8 -*-
 import sys
 import os
 import comtypes.client
+import re
 
 wdFormatPDF = 17
 
-in_file = os.path.abspath(sys.argv[1])
-out_file = os.path.abspath(sys.argv[2])
+filepath = os.path.abspath(os.getcwd())
 
-word = comtypes.client.CreateObject('Word.Application')
-doc = word.Documents.Open(in_file)
-doc.SaveAs(out_file, FileFormat=wdFormatPDF)
-doc.Close()
-word.Quit()
+filelist = os.listdir(filepath)
+
+for i in range (0,len(filelist)):
+	if 'doc' in filelist[i] or 'DOC'in filelist[i] :
+		if	'~$' not in filelist[i]:
+			f = os.path.join(filepath,filelist[i])
+			print(f)
+			if os.path.isfile(f):
+				print(f)
+				word = comtypes.client.CreateObject('Word.Application')
+				doc = word.Documents.Open(f)
+				#print(os.getcwd()+'\\'+ filelist[i]+'.pdf')
+				doc.SaveAs(os.getcwd()+'\\'+ filelist[i]+'.pdf', FileFormat=wdFormatPDF)
+				doc.Close()
+				word.Quit()
