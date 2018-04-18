0ed1ac6599897c7ceea438b20b1ec9b87c1ccec7
摸鱼 <jieyaren@gmail.com>
2018-04-18 16:21:23 +0800

    调整目录 重新分类

--- virtual_1704_at_home.bat
+++
@@ -1,7 +0,0 @@
-@echo u17 starting
-
-cd /d "C:\Program Files\Oracle\VirtualBox\"
-
-VBoxManage.exe startvm 1704 --type headless
-
-@echo off
--- virtual_ubuntu_4_compile.bat
+++
@@ -1,5 +0,0 @@
-@echo encrypt xp starting
-cd /d "C:\Program Files (x86)\VMware\VMware VIX\"
-
-vmrun.exe start "D:\ubuntu10_04\Ubuntu.vmx" nogui
-@echo off
