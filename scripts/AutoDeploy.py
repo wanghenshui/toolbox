3e035f5096c67a90c109cd2a0ebd4ceb147214f7
摸鱼 <jieyaren@gmail.com>
2018-05-16 17:41:12 +0800

    重写完成。还是很糙。就这样了。

--- scripts/AutoDeploy.py
+++ scripts/AutoDeploy.py
@@ -7,10 +7,12 @@ import logging
 import re
 import time
 import shutil
+import sqlite3
+import configparser
+
 now = time.strftime("%Y-%m-%d-%H-%M-%S")
 
 ##--------------log color--------------------
-## can't work
 def add_coloring_to_emit_windows(fn):
         # add methods we need to the class
     def _out_handle(self):
@@ -119,7 +121,7 @@ log=logging.getLogger()
 log.setLevel(logging.DEBUG)
                                 
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 ch.setFormatter(fmt)
 log.addHandler(ch)
                     
@@ -194,17 +196,30 @@ class FolderContainsBase(object):
             ret=self.channel.recv(65535)
             result+=str(ret)
             if tp.search(result) or mp.search(result) :
-                #log.debug ("execute finished")
+                log.debug ("execute finished")
                 break
+            
     #get sudo prio
     def sudo(self):
         self.channel.send('sudo su\r')
         time.sleep(0.5)
+
+    def chmod(self):
+        cmd = 'chmod 777 ' + remote_dir+'*'
+        log.info(cmd)
+        self.execute(cmd)
+        '''_,std_out,_ = self.ssh.exec_command('sudo su;'+cmd)
+        for x in std_out.readlines():
+            log.info(x.strip("\n"))'''
+        time.sleep(0.5)
         
     def killVOS(self):
-        log.info('kill VOS instance to restart')
         cmd = 'kill `pgrep -n VOS.exe`'
+        log.info(cmd)
         self.execute(cmd)
+        '''_,std_out,_ =self.ssh.exec_command('sudo su;'+cmd,get_pty=True)
+        for x in std_out.readlines():
+            log.info(x.strip("\n"))'''
         time.sleep(0.5)
 
     def backupfolder(self,dst):
@@ -212,6 +227,8 @@ class FolderContainsBase(object):
         log.info(cmd)
         self.execute(cmd)
         time.sleep(0.5)
+
+
     #sftp wrapper, upload files
     def base_upload(self,local_dir,remote_dir):
         log.info('uploading...to ' + remote_dir+' '+' from '+local_dir)
@@ -224,9 +241,9 @@ class FolderContainsBase(object):
             for filespath in files:
                 local_file = os.path.join(root,filespath)
                 a = local_file.replace(local_dir,'').replace('\\','/').lstrip('/')
-                log.info('%s',  remote_dir)
+                log.debug('%s',  remote_dir)
                 remote_file = os.path.join(remote_dir,a)
-                log.info('%s',remote_file)
+                log.debug('%s',remote_file)
                 try:
                     sftp.put(local_file,remote_file)
                     log.info('%s uploading',local_file)
@@ -237,7 +254,7 @@ class FolderContainsBase(object):
                     fh.close()
             for name in dirs: 
                 local_path = os.path.join(root,name)
-                log.info("%s,%s",local_path,local_dir)
+                log.debug("%s,%s",local_path,local_dir)
                 a = local_path.replace(local_dir,'').replace('\\','')
                 remote_path = os.path.join(remote_dir,a)
                 try:
@@ -253,8 +270,6 @@ class FolderContainsBase(object):
 
 
 class TscFiles(FolderContainsBase):
-    script='tsc_config.sh'
-    have_script=False
     def backup(self):
         self.sudo()
         log.info('TscFiles backup')
@@ -265,38 +280,205 @@ class TscFiles(FolderContainsBase):
         self.base_upload(local_dir,remote_dir)
         
     def invoke(self):
+        self.sudo()
         log.info('TscFiles invoke')
-        if have_script:           
-            self.killVOS()
-        
+        self.chmod()
+        self.killVOS()
+        shutil.copyfile(self.db_dir,self.db_dir+hostname+'.db')
+        shutil.copyfile(self.db_dir+".bak",self.db_dir)
+
+    #config db file        
     def prepare(self):
-        if os.path.isfile(local_dir+"\\" + scripts) is False:
-            log.info(u'请确保'+script +u'在文件夹下，否则无法修改配置文件')
+        TSC_IP = hostname
+        log.info('TscFile prepare....Config db....')
+        db='VOS.Config.db'
+        db_ver1 = local_dir+'\\'+db
+        db_ver2 = local_dir+'\\'+'data'+'\\'+db
+        conf=local_dir+'\\'+'AutoDeploy.ini'
+        if os.path.isfile(db_ver1):
+            log.warn('db file is '+db_ver1)
+            self.db_dir=db_ver1            
+        elif os.path.isfile(db_ver2):
+            log.warn('db file is '+db_ver2)
+            self.db_dir=db_ver2
         else:
-            have_script=True
+            log.error('where is db file????')
+            time.sleep(600)
+            quit(-1)
 
+        conn=sqlite3.connect(self.db_dir)
+        shutil.copyfile(self.db_dir,self.db_dir+".bak")
+        config=configparser.ConfigParser()
+        try:
+            config.read(conf)
+            TSC_ID=config.get('TSC','TSC_ID')
+            MSO_IP=config.get('TSC','MSO_IP')
+            DMR=config.get('TSC','DMR')
+        except:
+            log.error(u'注意AutoDeploy.ini文件必须保存为utf8无bom文件，请用notepad++操作，而不是使用Windows自带的notepad (win10notepad1803解决了这个问题)')
+            log.info(u'或者你可以按照提示手动输入需要配置的信息(参数无校验)')
+            TSC_ID=input(u"基站的网元编号(TSC_ID):\n")
+            MSO_IP=input(u"基站归属中控的ip:\n")
+            DMR= input("DmrCmpMode:\n")
+        finally:
+            log.info('tsc_ip=%s,tsc_id=%s,mso_ip=%s,dmr=%s', TSC_IP ,TSC_ID ,MSO_IP ,DMR)
 
+        sqlstr='''
+insert or replace into Tbl_Config (PID,Tag,Key,Value,Comment) VALUES(0,"VOS", "LOGVIEW/MAXCOUNT","10","");
+update Tbl_Config set Value='0xff004f1f' where Tag='VOS' and Key='LOG/LOGLEVEL';
+update Tbl_Config set Value='2/true/$TSC_IP:8000/$MSO_IP:6088' where Tag='VOS/SERVICES/IServices/TRT'  and Key='23:0';
+update Tbl_Config set Value='2/true/$TSC_IP:8002/$MSO_IP:5002' where Tag='VOS/SERVICES/IServices/TRT' and Key='36:28672';
+/*TSC 修改*/
+update Tbl_Config set Value='$TSC_ID'          where  Tag='TSC'and Key='TSC_ID';
+update Tbl_Config set Value='$DMR'           where  Tag='TSC'and Key='DMRCmptMode';
+update Tbl_Config set Value='$TSC_IP:0' where  Tag='TSC'and Key='TSC_ExtraNetIP';
+/* Agent 配置修改*/
+update Tbl_Config set Value='$MSO_IP' where  Tag='Agent'and Key='OM_SERVER_IP';
+update Tbl_Config set Value='$MSO_IP' where  Tag='Agent'and Key='OM_SLAVE_IP';
+update Tbl_Config set Value='$TSC_IP'  where  Tag='Agent'and Key='AGENT_IP';
+update Tbl_Config set Value='$TSC_ID'        where  Tag='Agent'and Key='TSC_ID';
+'''
+        sqlstr=sqlstr.replace('$MSO_IP',MSO_IP)
+        sqlstr=sqlstr.replace('$TSC_IP',TSC_IP)
+        sqlstr=sqlstr.replace('$TSC_ID',TSC_ID)
+        sqlstr=sqlstr.replace('$DMR',DMR)
+        
+        conn.executescript(sqlstr)
+        conn.commit()
+        conn.close()
+        
+        
 class MsoFiles(FolderContainsBase):
-    script='mso_config.sh'
-    have_script=False
     def backup(self):
         log.info('MsoFiles backup')
         self.backupfolder(remote_dir)
         
     def upload(self):
-        self.base_upload(local_dir,remote_dir)
         log.info('MsoFiles upload')
+        self.base_upload(local_dir,remote_dir)        
         
-    def invoke(self):
-        self.killVOS()
+    def invoke(self):        
         log.info('MsoFiles invoke')
+        self.chmod()
+        self.killVOS()
+        shutil.copyfile(self.db_dir,self.db_dir+hostname+'.db')
+        shutil.copyfile(self.db_dir+".bak",self.db_dir)
 
     def prepare(self):
-        if os.path.isfile(local_dir+"\\" + scripts) is False:
-            log.info(u'请确保'+script +u'在文件夹下，否则无法修改配置文件')
+        MSO_IP = hostname
+        log.info('MSO File prepare....Config db....')
+        db='VOS.Config.db'
+        db_ver1 = local_dir+'\\'+db
+        db_ver2 = local_dir+'\\'+'data'+'\\'+db
+        conf=local_dir+'\\'+'AutoDeploy.ini'
+        if os.path.isfile(db_ver1):
+            log.warn('db file is '+db_ver1)
+            self.db_dir=db_ver1
+        elif os.path.isfile(db_ver2):
+            log.warn('db file is '+db_ver2)
+            self.db_dir=db_ver2
         else:
-            have_script=True
+            log.error('where is db file????')
+            time.sleep(600)
+            quit(-1)
+
+        shutil.copyfile(self.db_dir,self.db_dir+".bak")
+        conn=sqlite3.connect(self.db_dir)
+        c=conn.cursor()
+        OLD_MSO_IP = c.execute("select Value from Tbl_Config  where  Tag='VOS/SERVICES/IServices/CRT'and Key='51:0x3000'")\
+                     .fetchone()[0].split('/')[-1].split(':')[0]
+        #('0/false/20.0.2.178:6088/20.0.2.178:8088',)
+        c.close()
         
+        config=configparser.ConfigParser()
+        try:
+            config.read(conf)
+            SystemID=config.get('MSO','SystemID')
+            PSTN_IP=config.get('MSO','PSTN_IP')
+            DMR=config.get('MSO','DMR')
+            CallMode=config.get('MSO','CallMode')
+            DialPlan=config.get('MSO','DialPlan')
+            TSC1_IP=config.get('MSO','TSC1_IP')
+            TSC2_IP=config.get('MSO','TSC2_IP')
+            TSC3_IP=config.get('MSO','TSC3_IP')
+            TSC1_ID=config.get('MSO','TSC1_ID')
+            TSC2_ID=config.get('MSO','TSC2_ID')
+            TSC3_ID=config.get('MSO','TSC3_ID')
+        except:
+            log.error(u'注意AutoDeploy.ini文件必须保存为utf8无bom文件，请用notepad++操作，而不是使用Windows自带的记事本 (win10 1803解决了这个问题)')
+            log.info(u'或者你可以按照提示手动输入需要配置的信息(参数无校验)')
+            SystemID=input(u"中控的SystemID:\n")
+            DMR= input("DmrCmpMode [1:DMR][0:PDT]:\n")
+            PSTN_IP=input("PSTN_IP:\n")
+            CallMode=input("CallMode? [4:DMR][3:PDT-P3][8:MPT&DMR]:\n")
+            DialPlan=input("Dial Plan [3,4,5,6,8] :\n")
+            TSC1_ID=input("TSC1_ID:\n")
+            TSC1_IP=input("TSC1_IP:\n")
+            TSC2_ID=input("TSC2_ID:\n")
+            TSC2_IP=input("TSC2_IP:\n")
+            TSC3_ID=input("TSC3_ID:\n")
+            TSC3_IP=input("TSC3_IP:\n")
+        finally:
+            pass
+            #log.info('MSO_IP=%,SystemID=%s,DMR=%s,PSTN_IP=%s,CallMode=%s，DialPlan=%s,TSC1_IP=%s,TSC2_IP=%s,TSC3_IP=%s' ,MSO_IP ,SystemID ,DMR ,PSTN_IP ,CallMode ,DialPlan ,TSC1_IP ,TSC2_IP ,TSC3_IP)
+        
+        sqlstr='''
+/*Log counts*/
+insert or replace INTO Tbl_Config (PID,Tag,Key,Value,Comment) VALUES(0,"VOS", "LOGVIEW/MAXCOUNT","10","");
+update Tbl_Config set value = replace (value,'$OLD_MSO_IP','$MSO_IP');
+/*PSTN*/
+update Tbl_Config set Value='$PSTN_IP:5060'           where  Tag='PSTNGW'and Key='PSTNIp';
+/*MTU*/
+update Tbl_Config set Value='1/false/0.0.0.0:7000/20.0.2.130:6080'           where  Tag='VOS/SERVICES/IServices/CRT'and Key='60:0x6000';
+/*system id*/
+update Tbl_Config set Value='$SystemID'           where  Tag='MSO'and Key='SystemID';
+/* DMR */
+update Tbl_Config set Value='$DMR'           where  Tag='MSO'and Key='DMRCmptMode';
+/* Call Mode*/
+update Tbl_Config set Value='$CallMode'      where  Tag='SAG' and Key='SYS_MODE';
+/* Dial Plan 6.5*/
+update Tbl_Config set Value='$DialPlan'	where Tag='MSO' and Key='DialPlan';
+/*SAG*/
+update Tbl_Config set value = replace (value,'192.168.20.96','$MSO_IP');
+/*SPGP*/
+update Tbl_Config set value = replace (value,'192.168.20.18','$MSO_IP');
+update Tbl_Config set value="$MSO_IP:6000" where tag = "SPGP" and key = "MsoIp";
+/*AISGW*/
+update Tbl_Config set value="$MSO_IP:5062" where tag = "AISGW" and key = "AISGWIP";
+/*GPSS*/
+update Tbl_Config set value="2/false/$MSO_IP:29876/$MSO_IP:0" where tag = "VOS/SERVICES/IServices/GPSS" and key = "56";
+/*vauc*/
+update Tbl_Config set value="2/false/$MSO_IP:5002/$MSO_IP:8002" where tag = "VOS/SERVICES/IServices/V_AuC" and key = "4";
+update Tbl_Config set value="$MSO_IP:3696" where tag = "V_AuC" and key = "AucUDPAddr";
+update Tbl_Config set value="$MSO_IP:5500" where tag = "V_AuC" and key = "KdmcUDPAddr";
+update Tbl_Config set value="$MSO_IP:3696" where tag = "V_AuC" and key = "LocalUDPAddr";
+update Tbl_Config set value="2/false/$MSO_IP:6088/$TSC1_IP:8000" where tag = "VOS/SERVICES/IServices/CRT" and key = "4:1";
+update Tbl_Config set key="4:$TSC1_ID" where tag = "VOS/SERVICES/IServices/CRT" and value = "2/false/$MSO_IP:6088/$TSC1_IP:8000";
+update Tbl_Config set value="2/false/$MSO_IP:6088/$TSC2_IP:8000" where tag = "VOS/SERVICES/IServices/CRT" and key = "4:2";
+update Tbl_Config set key="4:$TSC2_ID" where tag = "VOS/SERVICES/IServices/CRT" and value = "2/false/$MSO_IP:6088/$TSC2_IP:8000";
+update Tbl_Config set value="2/false/$MSO_IP:6088/$TSC3_IP:8000" where tag = "VOS/SERVICES/IServices/CRT" and key = "4:3";
+update Tbl_Config set key="4:$TSC3_ID" where tag = "VOS/SERVICES/IServices/CRT" and value = "2/false/$MSO_IP:6088/$TSC3_IP:8000";
+--replace INTO Tbl_Config (PID,Tag,Key,Value,Comment) VALUES(0,"VOS/SERVICES/IServices/CRT", "4:$TSC_ID", "2/false/$MSO_IP:6088/$TSC_IP:8000","");
+'''
+
+        sqlstr=sqlstr.replace('$OLD_MSO_IP',OLD_MSO_IP)
+        sqlstr=sqlstr.replace('$MSO_IP',MSO_IP)
+        sqlstr=sqlstr.replace('$SystemID',SystemID)
+        sqlstr=sqlstr.replace('$DMR',DMR)
+        sqlstr=sqlstr.replace('$PSTN_IP',PSTN_IP)
+        sqlstr=sqlstr.replace('$CallMode',CallMode)
+        sqlstr=sqlstr.replace('$DialPlan',DialPlan)
+        sqlstr=sqlstr.replace('$TSC1_IP',TSC1_IP)
+        sqlstr=sqlstr.replace('$TSC2_IP',TSC2_IP)
+        sqlstr=sqlstr.replace('$TSC3_IP',TSC3_IP)
+        sqlstr=sqlstr.replace('$TSC1_ID',TSC1_ID)
+        sqlstr=sqlstr.replace('$TSC2_ID',TSC2_ID)
+        sqlstr=sqlstr.replace('$TSC3_ID',TSC3_ID)
+        #log.info(sqlstr)
+        conn.executescript(sqlstr)
+        conn.commit()
+        conn.close()
+    
 class DbFiles(FolderContainsBase):
     def backup(self):
         log.info('DbFiles backup')
@@ -391,20 +573,33 @@ class AutoDeploy(FolderContainsBase):
 
 if __name__ == '__main__':
     print('-------------------------------------------')
-    print("|           TSC  AutoDeploy               |")
+    print("|                AutoDeploy               |")
     print('|                                         |')
     print('-------------------------------------------')
     print(u'请将该程序放在上传文件的同级目录下')
-    print(u'程序会将同目录下所有文件上传，所以整准点')
-    print(u'如果崩溃把日志rtx给w18858或者skype wangqw2ee@outlook.com')
+    print(u'如果是TSC文件或MSO文件请确保AutoDeploy.ini内容配置正确')
+    print(u'如果闪退把日志rtx:w18858 Or skype:wangqw2ee@outlook.com')
     set_logcolor()
-    hostname=input(u"ip (没有ip校验，别输错了):\n")
+    t_start=time.time()
+	
+    config=configparser.ConfigParser()
+    try:
+        config.read('AutoDeploy.ini')
+        hostname=config.get('COMM','SERVER_IP')
+    except:
+        log.error(u'注意AutoDeploy.ini文件必须保存为utf8无bom文件，请用notepad++操作，而不是使用Windows自带的notepad (win10notepad1803解决了这个问题)')
+        log.info(u'或者没有AutoDeploy.ini文件，没关系，你可以按照提示手动输入需要配置的信息(参数无校验)')
+    finally:
+        hostname_in=input(u"ip (没有ip校验，别输错了) 回车默认 %s:\n" %hostname)  
+        if hostname_in != '':
+            hostname = hostname_in
 
-    key=input(u"密码 默认vos:\n")
+    key=input(u"密码 回车默认vos:\n")
     if key != '':
         password=key
-
     
     AutoDeploy()
-    log.info ('complete %s' %now)
-    time.sleep(60)
+    t_end=time.time()
+    
+    log.info ('complete %s, all cost %d seconds' ,now ,(t_end-t_start))
+    time.sleep(6000)
