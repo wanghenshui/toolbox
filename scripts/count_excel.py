a1f61040acc0d5e081b9bdb3d4cacc0304e9748e
摸鱼 <jieyaren@gmail.com>
2018-07-03 17:27:14 +0800

    加图形界面

--- scripts/count_excel.py
+++ scripts/count_excel.py
@@ -1,12 +1,33 @@
 import xlrd
-<<<<<<< HEAD
-data = xlrd.open_workbook(r"C:\Users\Administrator\Desktop\xxx.xls")
-=======
-data = xlrd.open_workbook(r"C:\Users\Administrator\Desktop\R5.0联调用例.xls")
->>>>>>> 20fc19f4f75155c0a2627380883030bcd2c9a215
-s = 0;
-for i in range(len(data.sheets())):
-    table = data.sheets()[i]
-    s = s+table.nrows -1
+from tkinter import *
+from tkinter.filedialog import askdirectory,askopenfilename
+from tkinter import messagebox
 
-print(s)
+root = Tk()
+path = StringVar()
+
+def select_path():
+    path_ = askopenfilename()
+    path.set(path_)
+
+def count_excel():
+    try:
+        data = xlrd.open_workbook(path.get())
+        s = 0;
+        for i in range(len(data.sheets())):
+            table = data.sheets()[i]
+            s = s+table.nrows -1
+            #print(s)
+        messagebox.showinfo('总行数 ',s)
+    except Exception as e:        
+        messagebox.showwarning('警告',"文件是否正确？")
+        messagebox.showwarning('警告',e)
+    finally:
+        pass
+        
+root.title("count excel")
+Label(root,text = "Excel路径").grid(row=0,column=0)
+Entry(root,textvariable=path).grid(row=0,column=1)
+Button(root,text="选择文件路径",command = select_path).grid(row=0,column=2)
+Button(root,text="计算",command = count_excel).grid(row=0,column=3)
+root.mainloop()
