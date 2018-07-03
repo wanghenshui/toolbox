0c436e3f564cde6dfed0b910f5da5d11342e7f34
摸鱼 <jieyaren@gmail.com>
2018-07-03 20:01:45 +0800

    加上图形界面，去掉配置文件写法

--- scripts/compare_com.py
+++ scripts/compare_com.py
@@ -1,65 +1,44 @@
 #!/usr/bin/python
 # coding=utf-8
-#   头文件比对工具,头文件太TM乱了
-#   使用方法 path.ini里写好common头文件和本地头文件目录，点击执行
-CONFSTR='''
-;使用方法
-;安装python3.0以上的版本
-;[common]写common目录 [local]写本地common 目录
-;双击compare_com.py即可
-;
-[common]
-dir = E:\cccc
-
-[local]
-dir = E:\Br_R6.5_r2676_\COMM
-'''
-class configfile:
-    import os
-    file_name = os.getcwd() + '\\' + 'path.ini'
-
-    def detail(self):
-        return CONFSTR
-    
-    def is_conf_exist(self):
-        return self.os.path.isfile(self.file_name)
-    
-    def __init__(self):
-        if self.is_conf_exist() is not True:
-            f = open(self.file_name,'w')
-            f.write(self.detail())
-            f.close()
-
-#parser ini
-import configparser
-config=configparser.ConfigParser()
-config.read('path.ini','utf-8')
-common_dir = config.get('common','dir')
-local_dir = config.get('local','dir')
 
 #compare
 import filecmp
+import shutil
 
-#pause
-import time
+from tkinter import *
+from tkinter.filedialog import askdirectory
+from tkinter import messagebox
+
+root = Tk()
+path_local = StringVar()
+path_comm = StringVar()
+
+def select_local_path():
+    path_ = askdirectory()
+    path_local.set(path_)
+
+def select_comm_path():
+    path_ = askdirectory()
+    path_comm.set(path_)
 
 #补充反斜杠
 def add_black_slash(s):
-    if s[-1] is not '\\' :
-        s = s + '\\'
+    if s[-1] is not '/' :
+        s = s + '/'
     return s
 
-common_dir = add_black_slash(common_dir)
-local_dir = add_black_slash(local_dir)
 
-#通用打印
-#TODO 内部记录状态，闭包
-def print_file_list(com_dir,lo_dir,l):
+
+def dif_file_str(com_dir,lo_dir,l):
     if len(l):
-        print(u"在目录", lo_dir,u"下，以下文件和目录下",com_dir,u" 的文件不一致")
-        print(l)
-        print("")
-    
+        res= u"在目录"+ lo_dir+u"下，以下文件和目录下"+com_dir+u" 的文件不一致 \n"
+        for s in l:
+          res+=s;
+          res+="\n"
+        return res
+    else:
+        return ""
+
 
 '''
 +----------------+-------------+
@@ -75,61 +54,105 @@ def print_file_list(com_dir,lo_dir,l):
 
 #hardcode! 遍历 common目录，来比较本地目录下文件是否不同
 
-def compare_comm(common_d,local_d):
+def compare_comm(common_d,local_d,merge_flag=False):
     #1
-    TSC_MSO_Common = common_d+'TSC_MSO_Common\\'
-    TSC_MSO_H = local_d+'TSC_MSO_H\\'
+    ans=""
+    TSC_MSO_Common = common_d+'TSC_MSO_Common/'
+    TSC_MSO_H = local_d+'TSC_MSO_H/'
     dif = filecmp.cmpfiles(TSC_MSO_Common,TSC_MSO_H,['PDTHead.h','TSC_MSO_Common.h'])
-    print_file_list(TSC_MSO_Common,TSC_MSO_H,dif[1])
+    ans+=dif_file_str(TSC_MSO_Common,TSC_MSO_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(TSC_MSO_Common+d,TSC_MSO_H+d)
     
     #2
-    NM_Common = common_d+'NM_Common\\'
-    OM_H = local_d+'OM_H\\'
+    NM_Common = common_d+'NM_Common/'
+    OM_H = local_d+'OM_H/'
     d = filecmp.dircmp(NM_Common,OM_H)
-    print_file_list(NM_Common,OM_H,d.diff_files)
-
+    ans+=dif_file_str(NM_Common,OM_H,d.diff_files)
+    if merge_flag is True:
+        for d_ in d.diff_files:
+            shutil.copyfile(NM_Common+d_,OM_H+d_)
 
     #3
-    OTAR_Common = common_d+'OTAR_Common\\'
-    TSC_BRS_H = local_d+'TSC_BRS_H\\'
-    CTSC_BRS_H = common_d+'TSC_BRS_H\\'
+    OTAR_Common = common_d+'OTAR_Common/'
+    TSC_BRS_H = local_d+'TSC_BRS_H/'
+    CTSC_BRS_H = common_d+'TSC_BRS_H/'
     dif = filecmp.cmpfiles(OTAR_Common,TSC_BRS_H,['OTAR.h'])
-    print_file_list(OTAR_Common,TSC_BRS_H,dif[1])
+    ans+=dif_file_str(OTAR_Common,TSC_BRS_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(OTAR_Common+d,TSC_BRS_H+d)
 
     dif = filecmp.cmpfiles(CTSC_BRS_H,TSC_BRS_H,["TSC_BRS.h"])
-    print_file_list(CTSC_BRS_H,TSC_BRS_H,dif[1])
+    ans+=dif_file_str(CTSC_BRS_H,TSC_BRS_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(CTSC_BRS_H+d,TSC_BRS_H+d)
     
     #4
-    Auc_Common = common_d+'Auc_Common\\'
+    Auc_Common = common_d+'Auc_Common/'
     dif = filecmp.cmpfiles(Auc_Common,TSC_MSO_H,['Common_V_AuC.h'])
-    print_file_list(Auc_Common,TSC_MSO_H,dif[1])
+    ans+=dif_file_str(Auc_Common,TSC_MSO_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(Auc_Common+d,TSC_MSO_H+d)
 
     #5
-    PDU = common_d+'PDU\\'
-    COMM_H = local_d+'COMM_H\\'
+    PDU = common_d+'PDU/'
+    COMM_H = local_d+'COMM_H/'
     dif = filecmp.cmpfiles(PDU,COMM_H,['CCL_PDU.h','Common_Info.h'])
-    print_file_list(PDU,COMM_H,dif[1])
+    ans+=dif_file_str(PDU,COMM_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(PDU+d,COMM_H+d)
 
     #6
-    Common_Type = common_d+'Common_Type\\'
+    Common_Type = common_d+'Common_Type/'
     dif = filecmp.cmpfiles(Common_Type,COMM_H,['Common_Type.h'])
-    print_file_list(Common_Type,COMM_H,dif[1])
+    ans+=dif_file_str(Common_Type,COMM_H,dif[1])
+    if merge_flag is True:
+        for d in dif[1]:
+            shutil.copyfile(Common_Type+d,COMM_H+d)
+
+    if len(ans)==0:
+        ans="没有不同"
+    messagebox.showinfo("比对结果",ans)
+    
+        
 
      
+def compare(merge_flag=False):
+    common_dir = path_comm.get()
+    common_dir = add_black_slash(common_dir)
+    local_dir = path_local.get()
+    local_dir = add_black_slash(local_dir)
+    try:
+        compare_comm(common_dir,local_dir,merge_flag)
+    except:
+       messagebox.showwarning('警告',"文件目录是否正确？")
+    finally:
+        pass
 
-if __name__ == "__main__":
-    print(u'------TSC头文件比对工具--------')
-	c=configfile()
-    print(u'确保以下目录是正确的：')
-    print (u'    公共头文件目录: ',common_dir)
-    print (u'    本地common目录: ',local_dir)
-    print ("")
-	
-    print(u"比对开始！")
-    compare_comm(common_dir,local_dir)
-    print(u"比对结束！")
+def merge():
+    compare(True)
+        
+root.title("TSC头文件比对")
+Label(root,text = "本地头文件路径").grid(row=0,column=0)
+Label(root,text = "公共头文件路径").grid(row=1,column=0)
+Entry(root,textvariable=path_local).grid(row=0,column=1)
+Entry(root,textvariable=path_comm).grid(row=1,column=1)
+Button(root,text="选择路径",command = select_local_path).grid(row=0,column=2)
+Button(root,text="选择路径",command = select_comm_path).grid(row=1,column=2)
+Button(root,text="比对",command = compare).grid(row=0,column=3)
+Button(root,text="合并",command = merge).grid(row=1,column=3)
+root.mainloop()
 
-    time.sleep(600)
+
+
+
+
+
     
 
 
