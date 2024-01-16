f1feed6743a5a403d52e1042754a20db5705c08e
Ted Mosly <wanghenshui@qq.com>
2024-01-16 23:58:41 +0800

    github

--- old/py2exe.bat
+++
@@ -1,4 +0,0 @@
-::用法:将脚本文件直接拖入bat就可以
-@echo off
-pip install pyinstaller
-pyinstaller -F %1
--- old/virtual_box_mount.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-read -p "enter the SHARED FOLDER YOU SETTED:": enter_folder
-
-if [[ -ne /mnt/${enter_folder} ]]; then
-	mkdir /mnt/${enter_folder}
-
-mount -t vboxsf ${enter_folder} /mnt/${enter_folder}
-
-echo ${enter_folder} " /mnt/$enter_folder vboxsf rw,gid=110,uid=1100,auto 0 0 " >> /etc/fstab
